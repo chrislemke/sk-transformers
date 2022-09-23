67412556b51bf1981bf7bf94d4573aa09c7918c9
Prem Srinivasan <prem.srinivasan@invia.de>
2022-09-23 19:11:40 +0200

    Add unit tests for prepare_categorical_data

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,10 @@
 import pandas as pd
 import pytest
 
-from feature_reviser.utils import check_data
+from feature_reviser.utils import (
+    check_data,
+    prepare_categorical_data,
+)
 
 # pylint: disable=missing-function-docstring
 
@@ -34,3 +37,49 @@ def test_check_data_y_nan() -> None:
         check_data(pd.DataFrame([1, 2, 3]), pd.Series([1, None, 3]))
 
     assert "y must not contain NaN values!" == str(error.value)
+
+
+def test_prepare_categorical_data_x_type() -> None:
+    with pytest.raises(TypeError) as error:
+        prepare_categorical_data("wrong_type", [("a", 1), ("b", 2)])
+
+    assert "features must be a pandas.DataFrame!" == str(error.value)
+
+
+def test_prepare_categorical_data_x_value() -> None:
+    X = pd.DataFrame(
+        {
+            "c": [1, 2, 3],
+            "d": [4, 5, 6],
+            "e": [7, 8, 9],
+        }
+    )
+
+    with pytest.raises(ValueError) as error:
+        prepare_categorical_data(X, [("a", 1), ("b", 2)])
+
+    assert "cat_features must be in the dataframe!" == str(error.value)
+
+
+def test_prepare_categorical_data() -> None:
+    X = pd.DataFrame(
+        {
+            "a": ["A1", "A2", "A2", "A1", "A1", "A2", "A1", "A1"],
+            "b": [1, 2, 3, 4, 5, 6, 7, 8],
+            "c": [1, 2, 3, 1, 2, 3, 1, 3],
+            "d": ["1", "2", "3", "4", "5", "6", "7", "8"],
+        }
+    )
+
+    categories = [("a", 2), ("c", 3)]
+    result = prepare_categorical_data(X, categories).dtypes    
+    expected = pd.Series(
+        [
+            "category",
+            "float32",
+            "category",
+            "float32",
+        ],
+        index=X.columns
+    )
+    assert result.equals(expected)
