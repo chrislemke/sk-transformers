62143cbbe9e41da9bb8d856fe5fa90bcf9fc44f6
Chris Lemke <11752694+chrislemke@users.noreply.github.com>
2023-01-03 16:53:04 +0100

    add: to vec transformer

--- src/sk_transformers/deep_transformer.py
+++ src/sk_transformers/deep_transformer.py
@@ -13,6 +13,55 @@ from sk_transformers.utils import check_ready_to_transform
 
 
 class ToVecTransformer(BaseEstimator, TransformerMixin):
+    """
+    This transformer trains an [FT-Transformer](https://paperswithcode.com/method/ft-transformer)
+    using the [pytorch-widedeep package](https://github.com/jrzaurin/pytorch-widedeep) and extracts the embeddings
+    from its embedding layer. The output shape of the transformer is (number of rows,(`input_dim` * number of columns)).
+    Please refer to [this example](https://pytorch-widedeep.readthedocs.io/en/latest/examples/09_extracting_embeddings.html)
+    for pytorch_widedeep example on how to extract embeddings.
+
+    Example:
+    ```python
+    import numpy as np
+    import pandas as pd
+    from pytorch_widedeep.datasets import load_adult
+    from sk_transformers.deep_transformer import ToVecTransformer
+
+    df = load_adult(as_frame=True)
+    df["target"] = (df["income"].apply(lambda x: ">50K" in x)).astype(int)
+    df = df.drop(["income", "educational-num"], axis=1)
+
+    cat_cols, cont_cols = [], []
+    for col in df.columns:
+        if df[col].dtype == "O" or df[col].nunique() < 50 and col != "target":
+            cat_cols.append(col)
+        elif col != "target":
+            cont_cols.append(col)
+
+    target_col = "target"
+    target = df[target_col].to_numpy()
+
+    transformer = ToVecTransformer(cat_cols, cont_cols, verbose=0)
+    transformer.fit_transform(df, target).shape
+    ```
+    ```
+    (48842, 416)
+    ```
+
+    Args:
+        cat_embed_columns (List[str]): List of categorical columns to be embedded.
+        continuous_columns (List[str]): List of continuous columns.
+        n_epochs (int): Number of epochs to train the model.
+        batch_size (int): Batch size to train the model.
+        input_dim (int): The so-called *dimension of the model*.
+            Is the number of embeddings used to encode the categorical and/or continuous columns.
+        n_blocks (int): Number of FT-Transformer blocks.
+        n_heads (int): Number of attention heads per FT-Transformer block.
+        preprocessing_kwargs (Optional[Dict[str, Any]]): Keyword arguments to pass to the [`TabPreprocessor`](https://pytorch-widedeep.readthedocs.io/en/latest/pytorch-widedeep/preprocessing.html#pytorch_widedeep.preprocessing.tab_preprocessor.TabPreprocessor).
+        model_kwargs (Optional[Dict[str, Any]]): Keyword arguments to pass to the [`FTTransformer`](https://pytorch-widedeep.readthedocs.io/en/latest/pytorch-widedeep/model_components.html#pytorch_widedeep.models.tabular.transformers.ft_transformer.FTTransformer).
+        training_kwargs (Optional[Dict[str, Any]]): Keyword arguments to pass to the [`Trainer`](https://pytorch-widedeep.readthedocs.io/en/latest/pytorch-widedeep/trainer.html#pytorch_widedeep.training.Trainer).
+    """
+
     def __init__(
         self,
         cat_embed_columns: List[str],
@@ -20,8 +69,8 @@ class ToVecTransformer(BaseEstimator, TransformerMixin):
         n_epochs: int = 1,
         batch_size: int = 32,
         input_dim: int = 32,
-        n_heads: int = 4,
         n_blocks: int = 4,
+        n_heads: int = 4,
         verbose: int = 1,
         preprocessing_kwargs: Optional[Dict[str, Any]] = None,
         model_kwargs: Optional[Dict[str, Any]] = None,
@@ -42,7 +91,17 @@ class ToVecTransformer(BaseEstimator, TransformerMixin):
         self.tab_vec_: Optional[Tab2Vec] = None
         self.fitted_ = False
 
-    def fit(self, X: pd.DataFrame, y: NDArray):
+    def fit(self, X: pd.DataFrame, y: NDArray) -> "ToVecTransformer":
+        """
+        Fits the `ToVecTransformer`. The `TabPreprocessor` is fitted and the `FTTransformer` is trained.
+
+        Args:
+            X (pd.DataFrame): The input data.
+            y (NDArray): The target data.
+
+        Returns:
+            ToVecTransformer: The fitted transformer.
+        """
 
         self.fitted_ = True
 
@@ -84,7 +143,18 @@ class ToVecTransformer(BaseEstimator, TransformerMixin):
         self.tab_vec_ = Tab2Vec(model, preprocessor, return_dataframe=True)
         return self
 
-    def transform(self, X: pd.DataFrame):
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Transforms the input data and returns the embeddings.
+        The output shape is (number of rows,(`input_dim` * number of columns)).
+
+        Args:
+            X (pd.DataFrame): The input data.
+
+        Returns:
+            pd.DataFrame: The embeddings.
+        """
+
         if self.tab_vec_ is None:
             raise ValueError("Transformer must be fitted first!")
 
