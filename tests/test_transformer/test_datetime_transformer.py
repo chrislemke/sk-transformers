ade19327b43e5e42f05f6b87052e0262f783fb69
Chris Lemke <1@lemke.ai>
2023-01-23 15:43:37 +0100

    feat: add date columns transformer

--- tests/test_transformer/test_datetime_transformer.py
+++ tests/test_transformer/test_datetime_transformer.py
@@ -2,9 +2,96 @@ import numpy as np
 import pytest
 from sklearn.pipeline import make_pipeline
 
-from sk_transformers import DurationCalculatorTransformer, TimestampTransformer
+from sk_transformers import (
+    DateColumnsTransformer,
+    DurationCalculatorTransformer,
+    TimestampTransformer,
+)
 
 # pylint: disable=missing-function-docstring, missing-class-docstring
+
+
+def test_date_columns_transformer_in_pipeline(tiny_date_df):
+    pipeline = make_pipeline(DateColumnsTransformer(["a"]))
+    X = pipeline.fit_transform(tiny_date_df).drop("a", axis=1)
+    expected = np.array(
+        [
+            [
+                2021,
+                1,
+                1,
+                4,
+                1,
+                53,
+                1,
+                False,
+                True,
+                False,
+                True,
+                False,
+                True,
+                False,
+                False,
+            ],
+            [
+                2022,
+                2,
+                2,
+                2,
+                33,
+                5,
+                1,
+                False,
+                False,
+                False,
+                False,
+                False,
+                False,
+                False,
+                False,
+            ],
+            [
+                2023,
+                3,
+                3,
+                4,
+                62,
+                9,
+                1,
+                False,
+                False,
+                False,
+                False,
+                False,
+                False,
+                False,
+                False,
+            ],
+        ],
+        dtype=object,
+    )
+
+    assert np.array_equal(X.to_numpy(), expected)
+    assert pipeline.steps[0][0] == "datecolumnstransformer"
+    assert pipeline.steps[0][1].features == ["a"]
+    assert pipeline.steps[0][1].date_format == "%Y-%m-%d"
+    assert pipeline.steps[0][1].date_elements == [
+        "year",
+        "month",
+        "day",
+        "day_of_week",
+        "day_of_year",
+        "week_of_year",
+        "quarter",
+        "is_leap_year",
+        "is_month_start",
+        "is_month_end",
+        "is_quarter_start",
+        "is_quarter_end",
+        "is_year_start",
+        "is_year_end",
+        "is_weekend",
+    ]
 
 
 def test_duration_calculator_transformer_in_pipeline_seconds(X_time_values) -> None:
